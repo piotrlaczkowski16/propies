67470963ed56d0fdf69b686841bed4ade88b4ff5
piotr <pierter231@gmail.com>
2026-07-13 11:02:58 +0100

    array of object

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import React from "react";
 function App() {
     const students = [
-      "Rahim", 
-      "Karim", 
-      "Jamal", 
-      "alberto", 
-      "hasan", 
-      "falafel", 
-      "ceyrowsky", 
-      "NIEMCY!"
+      {name: "Rahim",
+       age: 20,
+       city: "city"
+      },
+
+      {name: "Karim",
+        age: 21,
+        city: "city2"
+      },
+      {name: "Jamal",
+        age: 16,
+        city: "bydgoszcz"
+      } ,
+      {name: "alberto",
+        age: 16,
+        city: "bydgoszcz3"
+      }  ,
+      {name: "hasan",
+        age: 26,
+        city: "bydgoszcz16"
+      } ,
+      {name: "falafel",
+        age: 56,
+        city: "city67"
+      } ,
+      {name: "ceyrowsky",
+        age: "yes",
+        city: "hawaii"
+      }  ,
+      {name: "NIEMCY!",
+        age: 500,
+        city: "berlin"
+      } ,
     ];
     const showStudents = ["falafel", "hasan"];
 
   return (
     <div>
-      {students
-        .filter((student) => student !== "falafel" && student !== "alberto")
-        .map((student, index) => (
-        <h2 key={index}>{student}</h2>
-        ))}
+      {students.map ((student, index) => (
+        <div>
+          <h2>{student.name}</h2>
+          <p>age:{student.age}</p>
+          <p>city: {student.city}</p>
+        </div>
+      ))}
     </div>
   );
 }
