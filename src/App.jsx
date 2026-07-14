78bf4990a55972e389054d405542764ef7496000
piotr <pierter231@gmail.com>
2026-07-14 10:15:40 +0100

    lajk

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [liked, setLiked] = useState(false);
 
   return(
     <div>
-    <h1>{count}</h1>
-    <div  style={{display:"flex", textAlign: "center"}}>
-    <button style={{width: "150px", height:"300px"}}
-      onClick={()=> setCount (count + 13)}
-      >
-        Increase
-      </button>
-      <button style={{width: "150px", height:"300px"}}
-      onClick={()=> setCount (0)}
-      >
-        clear
-      </button>
-      <button style={{width: "150px", height:"300px"}}
-      onClick={()=> setCount (count - 17)}
-      >
-        decrease
-      </button>
+    <button  style={{width:"400px", height:"300px"}} onClick={()=> setLiked(!liked)}>
+      {liked ? "Liked" : "Like"}
+    </button>
       
-      </div>
     </div>
   );
 }
